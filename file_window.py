b1ec86f7b849dcebf8c8f63222ea9ec6368ab553
Noah-rmbd <157590199+Noah-rmbd@users.noreply.github.com>
2024-02-08 13:38:10 +0100

    Update file_window.py

--- file_window.py
+++ file_window.py
@@ -2,7 +2,6 @@ from PyQt6.QtWidgets import *
 from graphs_tab import FenGraph
 from table_tab import TableWindow
 from stats_tab import StatsTab
-from IA_tab import *
 import pandas as pd
 
 class FileWindow(QWidget):
@@ -13,32 +12,30 @@ class FileWindow(QWidget):
         layout = QGridLayout()
         self.setLayout(layout)
 
-        if file_url[-4:]==".csv" :
+        if file_url[-4:] == ".csv":
             self.data_frame = pd.read_csv(file_url)
-        else :
+        else:
             self.data_frame = pd.read_excel(file_url)
 
         self.tab_table = TableWindow(self.data_frame)
         self.tab_graph = FenGraph(self.tab_table.data_frame)
-        self.stats = StatsTab(self.tab_table.data_frame)
-        self.tab_IA = IATab(self.tab_table.data_frame)
+        self.tab_stats = StatsTab(self.tab_table.data_frame)
 
-        self.tab_table.insert_after.clicked.connect(self.update_graph)
-        self.tab_table.delete_button.clicked.connect(self.update_graph)
+        self.tab_table.insert_after.clicked.connect(self.update_tabs)
+        self.tab_table.delete_button.clicked.connect(self.update_tabs)
 
         self.tabwidget = QTabWidget()
         self.tabwidget.addTab(self.tab_table, "Tableau")
         self.tabwidget.addTab(self.tab_graph, "Graphique")
-        self.tabwidget.addTab(self.tab_stats, "Stats")
-        self.tabwidget.addTab(self.tab_IA,"Algorithmes")
+        self.tabwidget.addTab(self.tab_stats,"Stats")
         layout.addWidget(self.tabwidget, 0, 0)
 
-    def update_graph(self):
+    def update_tabs(self):
         self.tab_graph = FenGraph(self.tab_table.data_frame)
         self.tab_stats = StatsTab(self.tab_table.data_frame)
         self.tabwidget.removeTab(2)
         self.tabwidget.removeTab(1)
         self.tabwidget.addTab(self.tab_graph, "Graphique")
-        self.tabwidget.addTab(self.stats, "Stats")
-
+        self.tabwidget.addTab(self.tab_stats, "Stats")
+        
 
