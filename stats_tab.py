a277543d0a6dcbc493da3bb7a47a890f2db4290a
Noah-rmbd <157590199+Noah-rmbd@users.noreply.github.com>
2024-02-08 13:38:43 +0100

    Update stats_tab.py

--- stats_tab.py
+++ stats_tab.py
@@ -7,11 +7,61 @@ class StatsTab(QWidget):
         title = QLabel("Stats")
 
         self.data_frame = data_frame
+        name_cols = list(self.data_frame.columns)
+        index_cols = [i for i in range(len(name_cols))]
+        cols_to_be_deleted = []
+
+        for col in index_cols:
+            if not self.is_float(self.data_frame.iat[0, col]):
+                cols_to_be_deleted.append(name_cols[col])
+                index_cols.remove(col)
+
+        for col in cols_to_be_deleted:
+            if col in name_cols:
+                name_cols.remove(col)
+
+
+
+        self.stats_widget = QTableWidget()
+
+        self.stats_widget.setColumnCount(len(name_cols))
+        self.stats_widget.setRowCount(2)
+        self.stats_widget.setHorizontalHeaderLabels(name_cols)
+        self.stats_widget.setVerticalHeaderLabels(["Mean", "Sd"])
+        #print(self.data_frame.columns)
+
+        for j in index_cols:
+            self.stats_widget.setItem(0, j, QTableWidgetItem(str(round(self.mean(j),4))))
+            self.stats_widget.setItem(1, j, QTableWidgetItem(str(round(self.sd(j), 4))))
+            #print(self.mean(j))
+        #print(self.data_frame.iat[0,1])
 
         tab.addWidget(title)
+        tab.addWidget(self.stats_widget)
         self.setLayout(tab)
 
     def mean(self, column):
-        mean = 0
         sum = 0
-        self.data_frame.iat[i, j]
+        num_rows = len(self.data_frame.index)
+        for i in range(num_rows):
+            sum += float(self.data_frame.iat[i, column])
+
+        mean = sum/num_rows
+        return mean
+
+    def sd(self, column):
+        mean = self.mean(column)
+        num_rows = len(self.data_frame.index)
+        sum = 0
+        for i in range(num_rows):
+            sum += abs(float(self.data_frame.iat[i, column])-mean)
+        sd = sum/num_rows
+        sd = pow(sd, 1/2)
+        return sd
+
+    def is_float(self, input_str):
+        try:
+            float(input_str)
+            return True  # Input is a valid float
+        except ValueError:
+            return False  # Input is not a valid float
