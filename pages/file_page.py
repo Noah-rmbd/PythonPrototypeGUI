1756473115dae875b4fec8e8c68679c563fa7922
Noah-rmbd <157590199+Noah-rmbd@users.noreply.github.com>
2024-03-27 16:30:33 +0100

    Update file_page.py

--- pages/file_page.py
+++ pages/file_page.py
@@ -8,51 +8,34 @@ from secondary_pages.data_analysis_tab import FenData
 from secondary_pages.IA_tab import IATab
 
 from components.progress_bar import ProgressBar
+from components.next_step_bar import NextStepBar
 
 import pandas as pd
 
 class FileWindow(QWidget):
-    def __init__(self, file_url):
+    def __init__(self, data_frame, loading_bar, normal_visualization):
         QWidget.__init__(self)
 
         layout = QVBoxLayout()
         self.progress_bar = ProgressBar()
-        self.next_step_bar = QHBoxLayout()
+        self.next_step_bar = NextStepBar()
         self.content_layout = QStackedLayout()
 
-        next_btn_style = (
-            'QPushButton::disabled{background-color : #e1e1e1; border:3px #e1e1e1}QPushButton::hover{background-color : #e1e1e1; border:1px solid #000000}QPushButton{background-color:#ffffff; border:1px solid #000000; color:black;}')
-        self.next_button = QPushButton("Next step ->")
-        self.prev_button = QPushButton("<- Prev step")
+        self.next_step_bar.next_button.clicked.connect(self.switch_step)
+        self.next_step_bar.prev_button.clicked.connect(self.previous_step)
 
-        self.next_button.setMinimumSize(100, 40)
-        self.next_button.setStyleSheet(next_btn_style)
-        self.next_button.clicked.connect(self.switch_step)
-
-        self.prev_button.setMinimumSize(100, 40)
-        self.prev_button.setStyleSheet(next_btn_style)
-        self.prev_button.clicked.connect(self.previous_step)
-        self.prev_button.hide()
-
-        if file_url[-4:] == ".csv":
-            self.data_frame = pd.read_csv(file_url)
-        else:
-            self.data_frame = pd.read_excel(file_url)
+        self.data_frame = data_frame
+        self.normal_visualization = normal_visualization
 
         #list_steps = []
         self.nbr_step = 0
 
-        data_visualization = DataVisualization(self.data_frame)
-        #list_steps.append(list_steps)
+        self.data_visualization = DataVisualization(self.data_frame, loading_bar, normal_visualization)
 
         self.data_frame_splited = []
         self.percentage_splited = 20
 
-        self.content_layout.addWidget(data_visualization)
-
-        self.next_step_bar.addWidget(self.prev_button)
-        self.next_step_bar.addWidget(self.next_button)
-        self.next_step_bar.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.content_layout.addWidget(self.data_visualization)
 
         layout.addLayout(self.progress_bar)
         layout.addLayout(self.content_layout)
@@ -63,7 +46,7 @@ class FileWindow(QWidget):
 
     def switch_step(self):
         if self.nbr_step == 0:
-            self.data_modification = DataModification(self.data_frame)
+            self.data_modification = DataModification(self.data_frame, self.normal_visualization, self.next_step_bar)
             self.data_modification.split_button.clicked.connect(self.enable_button)
             self.data_modification.back_button.clicked.connect(self.disable_button)
 
@@ -71,10 +54,10 @@ class FileWindow(QWidget):
 
             self.nbr_step += 1
             self.progress_bar.step2_button.setEnabled(True)
-            self.next_button.setEnabled(False)
+            self.next_step_bar.next_button.setEnabled(False)
             self.content_layout.setCurrentIndex(self.nbr_step)
 
-            self.prev_button.show()
+            self.next_step_bar.prev_button.show()
 
         elif self.nbr_step == 1:
             self.data_frame = self.data_modification.data_frame
@@ -105,8 +88,8 @@ class FileWindow(QWidget):
             self.content_layout.removeWidget(self.data_modification)
 
             self.progress_bar.step2_button.setEnabled(False)
-            self.next_button.setEnabled(True)
-            self.prev_button.hide()
+            self.next_step_bar.next_button.setEnabled(True)
+            self.next_step_bar.prev_button.hide()
 
         elif self.nbr_step == 2:
             self.content_layout.removeWidget(self.data_analysis)
@@ -120,8 +103,8 @@ class FileWindow(QWidget):
         self.content_layout.setCurrentIndex(self.nbr_step)
 
     def disable_button(self):
-        self.next_button.setEnabled(False)
+        self.next_step_bar.next_button.setEnabled(False)
 
     def enable_button(self):
-        self.next_button.setEnabled(True)
+        self.next_step_bar.next_button.setEnabled(True)
         #AJOUTER CODE QUI RECUPÈRE LA DATA SPLITÉE
