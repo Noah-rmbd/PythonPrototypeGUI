4f87e39197be3a8218dc899e2c978e2fe77447d2
Noah-rmbd <157590199+Noah-rmbd@users.noreply.github.com>
2024-03-27 16:31:23 +0100

    Update table_tab.py

--- secondary_pages/table_tab.py
+++ secondary_pages/table_tab.py
@@ -1,9 +1,10 @@
-from PyQt6.QtWidgets import QFileDialog, QLineEdit, QComboBox, QApplication, QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, QLabel, QVBoxLayout, QWidget, QHBoxLayout
-
+from PyQt6.QtWidgets import QDialog, QProgressBar, QFileDialog, QLineEdit, QComboBox, QApplication, QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, QLabel, QVBoxLayout, QWidget, QHBoxLayout
+from components.dataframe_table import DataframeTable
 
 class TableTab(QWidget):
-    def __init__(self, data_frame):
+    def __init__(self, data_frame, loading_bar):
         super().__init__()
+        self.loading_bar = loading_bar
 
         content_layout = QVBoxLayout()
         window_layout = QHBoxLayout()
@@ -11,25 +12,10 @@ class TableTab(QWidget):
         self.data_frame = data_frame
 
         self.label = QLabel("File Table")
-        self.table_widget = QTableWidget()
-
-        self.Showdata()
+        self.table_widget = DataframeTable(self.data_frame, self.loading_bar)
 
         content_layout.addWidget(self.label)
         content_layout.addWidget(self.table_widget)
 
         window_layout.addLayout(content_layout)
         self.setLayout(window_layout)
-
-    def Showdata(self):
-        num_rows = len(self.data_frame.index)
-        num_cols = len(self.data_frame.columns)
-        self.table_widget.setColumnCount(num_cols)
-        self.table_widget.setRowCount(num_rows)
-        self.table_widget.setHorizontalHeaderLabels(self.data_frame.columns)
-
-        for i in range(num_rows):
-            for j in range(num_cols):
-                self.table_widget.setItem(i,j, QTableWidgetItem(str(self.data_frame.iat[i,j])))
-
-        self.table_widget.resizeColumnsToContents()
