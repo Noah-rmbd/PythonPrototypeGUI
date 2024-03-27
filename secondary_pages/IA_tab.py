8f768ddcf7161b3c5e429023439030a4e0a9682b
Noah-rmbd <157590199+Noah-rmbd@users.noreply.github.com>
2024-03-27 16:35:28 +0100

    Update IA_tab.py

--- secondary_pages/IA_tab.py
+++ secondary_pages/IA_tab.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PyQt6.QtWidgets import *
-from secondary_pages.graphs_tab import FenGraph
-from secondary_pages.data_modification import TableWindow
+#from secondary_pages.graphs_tab import FenGraph
+#from secondary_pages.table_tab import TableTab
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -45,7 +45,7 @@ class IATab(QWidget):
 
         self.plot_button = QPushButton("Plot")
         self.plot_button.clicked.connect(self.plot_based_on_selection)
-        self.plot_button.setEnabled(False)
+        #self.plot_button.setEnabled(False)
 
         label = QLabel()
         label.setText("Utiliser des algorithmes de machine learning")
@@ -75,10 +75,13 @@ class IATab(QWidget):
         self.criterian_combo.currentIndexChanged.connect(self.criterion_update)
 
         self.insert_neighbors_nb = None
-        self.k_value_as_int = None
-        self.tree_value_as_int = None
+        self.k_value_as_int = 5
+        self.tree_value_as_int = 100
         self.insert_tree_number = None
-        self.actual_criterion = None
+        self.actual_criterion = "gini"
+        self.mlp_activation = "relu"
+        self.learning_rate = "constant"
+
 
         ##################################################################
 
@@ -153,16 +156,16 @@ class IATab(QWidget):
 
     def plot_cart(self):
         test_size = float(self.test_size_box.currentText()[:2]) / 100
+        # if self.actual_criterion is not None:
+        #     cart_classifier = DecisionTreeClassifier(criterion=self.actual_criterion)
+        #     print("used criterion:", self.actual_criterion)
+        # else:
+        #     # Default to KNeighborsClassifier with default parameters
+        #     cart_classifier = DecisionTreeClassifier()
+        #     print("used criterion: default")
 
-        if self.actual_criterion is not None:
-            cart_classifier = DecisionTreeClassifier(criterion=self.actual_criterion)
-            print("used criterion:", self.actual_criterion)
-        else:
-            # Default to KNeighborsClassifier with default parameters
-            cart_classifier = DecisionTreeClassifier()
-            print("used criterion: default")
-
-        canvas = plot_algorithm_result(cart_classifier, self.matrice_sans_label, self.label_array, "Cart Algorithm", test_size)
+        canvas = plot_algorithm_result("DecisionTreeClassifier", self.matrice_sans_label, self.label_array, "Cart Algorithm", test_size,
+                                       criterion_in= self.actual_criterion)
         for i in reversed(range(self.canvas_layout.count())):
             self.canvas_layout.itemAt(i).widget().setParent(None)
 
@@ -173,14 +176,15 @@ class IATab(QWidget):
     def plot_KNN(self):
         test_size = float(self.test_size_box.currentText()[:2]) / 100
 
-        # Check if self.k_value_as_int is not None and greater than 0 before creating the classifier
-        if self.k_value_as_int is not None and self.k_value_as_int > 0:
-            knn_classifier = KNeighborsClassifier(n_neighbors=self.k_value_as_int)
-        else:
-            # Default to KNeighborsClassifier with default parameters
-            knn_classifier = KNeighborsClassifier()
+        # # Check if self.k_value_as_int is not None and greater than 0 before creating the classifier
+        # if self.k_value_as_int is not None and self.k_value_as_int > 0:
+        #     knn_classifier = KNeighborsClassifier(n_neighbors=self.k_value_as_int)
+        # else:
+        #     # Default to KNeighborsClassifier with default parameters
+        #     knn_classifier = KNeighborsClassifier()
 
-        canvas = plot_algorithm_result(knn_classifier, self.matrice_sans_label, self.label_array, "KNN Algorithm", test_size)
+        canvas = plot_algorithm_result("KNeighborsClassifier", self.matrice_sans_label, self.label_array, "KNN Algorithm", test_size,
+                                       neighbors_nb= self.k_value_as_int)
         for i in reversed(range(self.canvas_layout.count())):
             self.canvas_layout.itemAt(i).widget().setParent(None)
 
@@ -189,8 +193,9 @@ class IATab(QWidget):
 
     def plot_random_forest(self):
         test_size = float(self.test_size_box.currentText()[:2]) / 100
-        canvas = plot_algorithm_result(RandomForestClassifier, self.matrice_sans_label, self.label_array,
-                              "Random Forest Algorithm", test_size)
+        canvas = plot_algorithm_result("RandomForestClassifier", self.matrice_sans_label, self.label_array,
+                              "Random Forest Algorithm", test_size,
+                                tree_nb=self.tree_value_as_int, criterion_in=self.actual_criterion)
 
         for i in reversed(range(self.canvas_layout.count())):
             self.canvas_layout.itemAt(i).widget().setParent(None)
@@ -200,8 +205,9 @@ class IATab(QWidget):
 
     def plot_MLP(self):
         test_size = float(self.test_size_box.currentText()[:2]) / 100
-        canvas = plot_algorithm_result(MLPClassifier, self.matrice_sans_label, self.label_array,
-                              "MLP algorithm", test_size)
+        canvas = plot_algorithm_result("MLPClassifier", self.matrice_sans_label, self.label_array,
+                              "MLP algorithm", test_size,
+                              mlp_solver= self.solver_type, mlp_learning_rate= self.learning_rate, mlp_activation=self.mlp_activation)
         for i in reversed(range(self.canvas_layout.count())):
             self.canvas_layout.itemAt(i).widget().setParent(None)
 
@@ -306,24 +312,72 @@ class IATab(QWidget):
         self.criterian_combo.currentIndexChanged.connect(self.criterion_update)
         self.fen_hyperpara_RF.exec()
 
+        print(self.actual_criterion)
+
     def hyperpara_MLP(self):
         self.fen_hyperpara_MLP.setWindowTitle("MLP Hyperparameter")
 
-        activation_combo = QComboBox()
-        activation_combo.addItem("Identify")
-        activation_combo.addItem("Logistic")
-        activation_combo.addItem("Hyperbolic tan")
-        activation_combo.addItem("Rectified linear unit (default)")
+        activation_label =QLabel("Activation function :")
+        self.activation_combo = QComboBox()
+        self.activation_combo.setPlaceholderText("Choose activation function")
+        self.activation_combo.addItem("Rectified linear unit (default)")
+        self.activation_combo.addItem("Identify")
+        self.activation_combo.addItem("Logistic")
+        self.activation_combo.addItem("Hyperbolic tan")
+        self.activation_combo.currentIndexChanged.connect(self.MLP_criterion_update)
+
+        solver_label =QLabel("Choose solver type:")
+        self.solver_type_combo = QComboBox()
+        self.solver_type_combo.setPlaceholderText("Choose solver type")
+        self.solver_type_combo.addItem("adam (default)")
+        self.solver_type_combo.addItem("sgd")
+        self.solver_type_combo.currentIndexChanged.connect(self.MLP_criterion_update)
+
+        learning_rate_label = QLabel ("Choose learning rate")
+        self.learning_rate_combo = QComboBox()
+        self.learning_rate_combo.setPlaceholderText("Choose learning rate")
+        self.learning_rate_combo.addItem("constant (default)")
+        self.learning_rate_combo.addItem("invscaling")
+        self.learning_rate_combo.addItem("adaptative")
+        self.learning_rate_combo.currentIndexChanged.connect(self.MLP_criterion_update)
 
         ok_button = QPushButton("OK")
         ok_button.clicked.connect(self.fen_hyperpara_MLP.accept)
 
         layout = QVBoxLayout()
-        layout.addWidget(activation_combo)
+        layout.addWidget(activation_label)
+        layout.addWidget(self.activation_combo)
+        layout.addWidget(solver_label)
+        layout.addWidget(self.solver_type_combo)
+        layout.addWidget(learning_rate_label)
+        layout.addWidget(self.learning_rate_combo)
+
         layout.addWidget(ok_button)
+
         self.fen_hyperpara_MLP.setLayout(layout)
         self.fen_hyperpara_MLP.setGeometry(500, 200, 300, 300)
         self.fen_hyperpara_MLP.exec()
+
+    def process_neighbors_input(self):
+        k_value = self.insert_neighbors_nb.text()
+        if k_value:
+            # Assuming you want to convert the input to an integer
+            self.k_value_as_int = int(k_value)
+            # Use the value_as_int in your function or logic
+            print(f"Entered value: {self.k_value_as_int}")
+
+    def process_tree_input(self):
+        print("Entered process_tree_input method")
+        try:
+            if self.insert_tree_number:
+                tree_value = self.insert_tree_number.text()
+                if tree_value:
+                    # Assuming you want to convert the input to an integer
+                    self.tree_value_as_int = int(tree_value)
+                    # Use the value_as_int in your function or logic
+                    print(f"Entered value: {self.tree_value_as_int}")
+        except Exception as e:
+            print(f"Exception in process_tree_input: {e}")
 
     def criterion_update(self):
         if self.criterian_combo.currentText() == "Gini (default)":
@@ -337,7 +391,25 @@ class IATab(QWidget):
 
         print(self.actual_criterion)
 
-# app = QApplication([])
-# window = IATab()
-# window.show()
-# sys.exit(app.exec())
+    def MLP_criterion_update(self):
+
+        if self.activation_combo.currentText() =="Identity":
+            self.mlp_activation = "identity"
+        elif self.activation_combo.currentText() == "Logistic":
+            self.mlp_activation = "logistic"
+        elif self.activation_combo.currentText() == "Hyperbolic tan":
+            self.mlp_activation = "tanh"
+        elif self.activation_combo.currentText() == "Rectified linear unit (default)":
+            self.mlp_activation = "relu"
+
+        if self.solver_type_combo.currentText() == "adam (default)":
+            self.solver_type = "adam"
+        elif self.solver_type_combo.currentText() == "sgd":
+            self.solver_type = "sgd"
+
+        if self.learning_rate_combo.currentText() == "constant (default)":
+            self.learning_rate = "constant"
+        elif self.learning_rate_combo.currentText() == "invscaling":
+            self.learning_rate = "invscaling"
+        elif self.learning_rate_combo.currentText() == "adaptative":
+            self.learning_rate = "adaptative"
