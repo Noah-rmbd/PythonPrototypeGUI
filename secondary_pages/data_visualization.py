654a2190d02e81d168c9bd34a7676c4a3f58e5c9
Noah-rmbd <157590199+Noah-rmbd@users.noreply.github.com>
2024-03-27 16:31:02 +0100

    Update data_visualization.py

--- secondary_pages/data_visualization.py
+++ secondary_pages/data_visualization.py
@@ -5,17 +5,18 @@ from secondary_pages.stats_tab import StatsTab
 
 
 class DataVisualization(QTabWidget):
-    def __init__(self, data_frame):
+    def __init__(self, data_frame, loading_bar, normal_visualization):
         super().__init__()
         self.data_frame = data_frame
 
-        tab_table = TableTab(self.data_frame)
+        tab_table = TableTab(self.data_frame, loading_bar)
         tab_graph = FenGraph(self.data_frame)
-        tab_stats = StatsTab(self.data_frame)
+        tab_stats = StatsTab(self.data_frame, normal_visualization)
 
 
         self.addTab(tab_table, "Table")
         self.addTab(tab_graph, "Graphs")
         self.addTab(tab_stats, "Stats")
 
+        self.table_widget = tab_table.table_widget
         self.setTabPosition(QTabWidget.TabPosition.South)
