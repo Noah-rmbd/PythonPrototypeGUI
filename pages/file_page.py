9e98e30772a82e037f1bfdd2d641b7acf915f59d
Noah-rmbd <157590199+Noah-rmbd@users.noreply.github.com>
2024-05-05 15:12:14 +0200

    Update file_page.py

--- pages/file_page.py
+++ pages/file_page.py
@@ -1,7 +1,4 @@
 from PyQt6.QtWidgets import *
-from PyQt6.QtCore import Qt
-import sys
-from PyQt6.QtGui import *
 from secondary_pages.data_visualization import DataVisualization
 from secondary_pages.data_modification import DataModification
 from secondary_pages.data_analysis_tab import FenData
@@ -9,7 +6,6 @@ from secondary_pages.feature_PCA import FenPCA
 from secondary_pages.data_training import DataTraining
 from secondary_pages.data_testing import DataTesting
 from secondary_pages.data_evaluation import DataEvaluation
-from secondary_pages.IA_tab import IATab
 
 from components.progress_bar import ProgressBar
 from components.next_step_bar import NextStepBar
@@ -18,12 +14,11 @@ import pandas as pd
 import numpy as np
 
 class FileWindow(QWidget):
-    def __init__(self, data_frame, loading_bar, normal_visualization):
+    def __init__(self, data_frame, loading_bar):
         QWidget.__init__(self)
 
-        window_style = (
-            'background-color:white; border-radius: 10px 0px 0px 0px;'
-        )
+        #Generates the page elements
+        window_style = 'background-color:white; border-radius: 10px 0px 0px 0px;'
 
         layout = QVBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
@@ -39,19 +34,24 @@ class FileWindow(QWidget):
         self.progress_bar_container.setStyleSheet(window_style)
         self.progress_bar_container.setLayout(self.progress_bar)
 
+        #self.next_step_bar.prev_button.setText("<- Homepage")
+        #self.next_step_bar.prev_button.show()
         self.next_step_bar.next_button.clicked.connect(self.switch_step)
         self.next_step_bar.prev_button.clicked.connect(self.previous_step)
 
+        #Generates the data structure
         self.data_frame = data_frame
-        self.normal_visualization = normal_visualization
+        self.percentage_splited = None
+        self.data_frame_splited = []
+        self.data_frame_features_selection = []
+        self.features_selection_percentage = None
+        self.data_frame_pca = []
+        self.pca_percentage = None
+        self.trained_model = None
 
-        #list_steps = []
         self.nbr_step = 0
 
-        self.data_visualization = DataVisualization(self.data_frame, loading_bar, normal_visualization)
-
-        self.data_frame_splited = []
-        self.percentage_splited = 20
+        self.data_visualization = DataVisualization(self.data_frame, loading_bar)
 
         self.content_layout.addWidget(self.data_visualization)
 
@@ -64,7 +64,7 @@ class FileWindow(QWidget):
 
     def switch_step(self):
         if self.nbr_step == 0:
-            self.data_modification = DataModification(self.data_frame, self.normal_visualization, self.next_step_bar)
+            self.data_modification = DataModification(self.data_frame, self.next_step_bar)
             self.data_modification.split_button.clicked.connect(self.enable_button)
             self.data_modification.back_button.clicked.connect(self.disable_button)
 
@@ -76,11 +76,15 @@ class FileWindow(QWidget):
             self.content_layout.setCurrentIndex(self.nbr_step)
 
             self.next_step_bar.prev_button.show()
+            #self.next_step_bar.prev_button.setText("<- Prev step")
+            print(self.nbr_step)
 
         elif self.nbr_step == 1:
-            self.data_frame = self.data_modification.data_frame
+            #self.data_frame = self.data_modification.data_frame
             self.data_frame_splited = [self.data_modification.X_train, self.data_modification.X_test,
                                        self.data_modification.y_train, self.data_modification.y_test]
+            self.data_frame_features_selection = self.data_frame_splited.copy()
+            self.data_frame_pca = self.data_frame_splited.copy()
             self.percentage_splited = int(self.data_modification.split_combo.currentText()[:2]) / 100
 
             #modifier self.data_frame pour que la colonne label soit automatiquement à la fin
@@ -95,13 +99,16 @@ class FileWindow(QWidget):
             print(self.percentage_splited)
 
         elif self.nbr_step == 2:
+            #Saves new data
             if self.data_analysis.index_features_to_del is not None:
                 self.index_features_to_del = self.data_analysis.index_features_to_del
-                self.data_frame_splited[0] = np.delete(self.data_frame_splited[0], self.index_features_to_del, axis=1)
-                self.data_frame_splited[1] = np.delete(self.data_frame_splited[1], self.index_features_to_del, axis=1)
-            self.data_frame_splited_version_features_selection = self.data_frame_splited.copy()
+                self.data_frame_features_selection[0] = np.delete(self.data_frame_splited[0], self.index_features_to_del, axis=1)
+                self.data_frame_features_selection[1] = np.delete(self.data_frame_splited[1], self.index_features_to_del, axis=1)
+            self.data_frame_pca = self.data_frame_features_selection.copy()
+            self.features_selection_percentage = self.data_analysis.seuil
 
-            self.pca_selection = FenPCA(self.data_frame_splited)
+            #Generates the page
+            self.pca_selection = FenPCA(self.data_frame_features_selection)
             self.content_layout.addWidget(self.pca_selection)
 
             self.nbr_step += 1
@@ -109,10 +116,13 @@ class FileWindow(QWidget):
             self.content_layout.setCurrentIndex(self.nbr_step)
 
         elif self.nbr_step == 3:
-            self.data_frame_splited[0] = self.pca_selection.X_train
-            self.data_frame_splited[1] = self.pca_selection.X_test
+            #Saves new data
+            self.data_frame_pca[0] = self.pca_selection.X_train
+            self.data_frame_pca[1] = self.pca_selection.X_test
+            self.pca_percentage = self.pca_selection.pct_variance_to_keep
 
-            self.data_training = DataTraining(self.data_frame_splited)
+            #Generates the page
+            self.data_training = DataTraining(self.data_frame_pca, self.next_step_bar)
             self.content_layout.addWidget(self.data_training)
 
             self.nbr_step += 1
@@ -121,8 +131,10 @@ class FileWindow(QWidget):
 
         elif self.nbr_step == 4:
             self.classifier = self.data_training.classifier
+            self.name_model = self.data_training.algorithm_name
+            self.hyperparameters = self.data_training.hyperparameters
 
-            self.data_testing = DataTesting(self.data_frame_splited, self.classifier)
+            self.data_testing = DataTesting(self.data_frame_pca, self.classifier, self.name_model, self.hyperparameters)
             self.content_layout.addWidget(self.data_testing)
 
             self.nbr_step += 1
@@ -130,7 +142,7 @@ class FileWindow(QWidget):
             self.content_layout.setCurrentIndex(self.nbr_step)
 
         elif self.nbr_step == 5:
-            self.data_evaluation = DataEvaluation(self.data_frame_splited)
+            self.data_evaluation = DataEvaluation(self.data_frame_pca, self.classifier, self.name_model, self.hyperparameters, self.percentage_splited, self.features_selection_percentage, self.pca_percentage, self.next_step_bar)
             self.content_layout.addWidget(self.data_evaluation)
 
             self.nbr_step += 1
@@ -153,18 +165,12 @@ class FileWindow(QWidget):
             self.content_layout.removeWidget(self.data_analysis)
             self.progress_bar.step3_button.setEnabled(False)
 
-            self.data_frame_splited[0] = self.data_modification.X_train
-            self.data_frame_splited[1] = self.data_modification.X_test
-
             self.nbr_step -= 1
             self.content_layout.setCurrentIndex(self.nbr_step)
 
         elif self.nbr_step == 3:
             self.content_layout.removeWidget(self.pca_selection)
             self.progress_bar.step4_button.setEnabled(False)
-
-            self.data_frame_splited[0] = self.data_frame_splited_version_features_selection[0].copy()
-            self.data_frame_splited[1] = self.data_frame_splited_version_features_selection[1].copy()
 
             self.nbr_step -= 1
             self.content_layout.setCurrentIndex(self.nbr_step)
@@ -178,6 +184,7 @@ class FileWindow(QWidget):
 
         elif self.nbr_step == 5:
             self.content_layout.removeWidget(self.data_testing)
+            self.next_step_bar.next_button.setText("Next Step ->")
             self.progress_bar.step6_button.setEnabled(False)
 
             self.nbr_step -= 1
@@ -195,4 +202,3 @@ class FileWindow(QWidget):
 
     def enable_button(self):
         self.next_step_bar.next_button.setEnabled(True)
-        #AJOUTER CODE QUI RECUPÈRE LA DATA SPLITÉE
